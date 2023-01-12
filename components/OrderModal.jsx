5ab382de20c6300875826c0aa9e87b7b344772c3
Nicholaus Adhyatma <nicholausadhyatma@gmail.com>
2023-01-12 17:41:57 +0700

    responsive-cart

--- components/OrderModal.jsx
+++ components/OrderModal.jsx
@@ -1,16 +1,35 @@
 import { Modal, useMantineTheme } from "@mantine/core";
 import { useState, useEffect } from "react";
+import { toast } from "react-toastify";
 import css from "../styles/OrderModal.module.css";
 
 const OrderModal = ({ opened, setOpened, paymentMethod }) => {
   const theme = useMantineTheme();
   const [total, setTotal] = useState(undefined);
+  const [formData, setFormData] = useState({});
 
   useEffect(() => {
     if (typeof window !== "undefined") {
       setTotal(localStorage.getItem("total"));
     }
   }, [opened]);
+
+  const handleInput = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+    toast.success("Pesanan di proses", {
+      position: "top-right",
+      autoClose: 2000,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+    });
+  };
 
   return (
     <Modal
@@ -25,10 +44,27 @@ const OrderModal = ({ opened, setOpened, paymentMethod }) => {
       onClose={() => setOpened(null)}
     >
       {/* Modal content */}
-      <form className={css.formContainer}>
-        <input type="text" name="name" required placeholder="Name" />
-        <input type="text" name="phone" required placeholder="Phone Number" />
-        <textarea name="address" rows="3" placeholder="Address"></textarea>
+      <form onSubmit={handleSubmit} className={css.formContainer}>
+        <input
+          type="text"
+          onChange={handleInput}
+          name="name"
+          required
+          placeholder="Name"
+        />
+        <input
+          type="text"
+          onChange={handleInput}
+          name="phone"
+          required
+          placeholder="Phone Number"
+        />
+        <textarea
+          name="address"
+          onChange={handleInput}
+          rows="3"
+          placeholder="Address"
+        ></textarea>
         <span>
           Anda akan membayar <span>Rp {total}</span>
         </span>
