70a7ad11cacef8248a042b63776a47b9d1bc48bc
Nicholaus Adhyatma <nicholausadhyatma@gmail.com>
2023-01-15 23:06:30 +0700

    complete#2

--- pages/success.js
+++ pages/success.js
@@ -1,9 +1,11 @@
+import { useState } from "react";
 import Layout from "../components/Layout";
 import OrderModal from "../components/OrderModal";
 
 export default function success() {
+  const [payment, setPayment] = useState(null);
   return(
     <Layout>
-      <OrderModal opened={true} paymentMethod={1}/>
+      <OrderModal opened={true} paymentMethod={1} setOpened={setPayment}/>
     </Layout>)
 };
