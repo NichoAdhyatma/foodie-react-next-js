66d24d0e5c05618370525cb4a7fa0606cee37d33
Nicholaus Adhyatma <nicholausadhyatma@gmail.com>
2023-01-16 12:41:57 +0700

    clear

--- pages/success.js
+++ pages/success.js
@@ -3,7 +3,7 @@ import Layout from "../components/Layout";
 import OrderModal from "../components/OrderModal";
 
 export default function success() {
-  const [payment, setPayment] = useState(null);
+  const [payment, setPayment] = useState(1);
   return(
     <Layout>
       <OrderModal opened={true} paymentMethod={1} setOpened={setPayment}/>
